d37b612427622f1d73e2c9eaa4ae6f43ed719cc3
井上大誠 <arinc0@inoueoomakotonoMacBook-Pro.local>
2022-08-02 20:32:58 +0900

    :art: change structure

---
+++ socket/client.go
@@ -0,0 +1,18 @@
+package docker
+
+import(
+	"io"
+	"os"
+	"fmt"
+	"net"
+)
+
+func Client() {
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Printf("connection error: %v\n", err.Error())
+		return
+	}
+	go func() {io.Copy(conn, os.Stdin)}()
+	io.Copy(os.Stdout, conn)
+}
